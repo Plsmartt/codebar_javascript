a3b35217fa3b92e12f2e75381f27921036ec13df
Plsmartt <39807612+Plsmartt@users.noreply.github.com>
2018-05-31 20:07:07 +0100

    First fuction

---
+++ week1/script.js
@@ -0,0 +1,9 @@
+// This is where all the practise code from the Introduction to Javascript tutorial goes
+
+console.log('hello world!');
+
+function sayHello() {
+	console.log('hello!'); 
+}
+sayHello();
+
